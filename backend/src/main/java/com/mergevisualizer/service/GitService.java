1c4baa83afea6b07bcbfcb15c2e9d4ba618301d3
jayant1906 <jayant.saini.indus@gmail.com>
2026-07-26 21:00:59 +0530

    some fixed for phase 3

--- backend/src/main/java/com/mergevisualizer/service/GitService.java
+++ backend/src/main/java/com/mergevisualizer/service/GitService.java
@@ -8,8 +8,11 @@ import org.eclipse.jgit.lib.Repository;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.List;
 
 @Service
@@ -19,7 +22,9 @@ public class GitService {
     }
 
     public List<BranchInfo> getBranches(Path repositoryPath) throws IOException, GitAPIException {
-        try(Git git = Git.open(repositoryPath.toFile())){
+        Path repoRoot = findRepositoryRoot(repositoryPath);
+
+        try(Git git = Git.open(repoRoot.toFile())){
             Repository repository = git.getRepository();
             List<Ref> branches = git.branchList().call();
             String currBranch = repository.getBranch();
@@ -35,5 +40,28 @@ public class GitService {
             }
             return branchInfo;
         }
+    }
+
+    private Path findRepositoryRoot(Path extractedFolder) throws IOException{
+        Path gitFolder = extractedFolder.resolve(".git");
+        if (Files.isDirectory(gitFolder)) {
+            return extractedFolder;
+        }
+        try (Stream<Path> entries = Files.list(extractedFolder)){
+            List<Path> subDirs = entries.filter(Files::isDirectory).collect(Collectors.toList());
+            for(Path dir:subDirs){
+                Path tempGitFolder = dir.resolve(".git");
+                if (Files.isDirectory(tempGitFolder)){
+                    return dir;
+                }
+            }
+            for (Path dir: subDirs){
+                Path result = findRepositoryRoot(dir);
+                if (result != null){
+                    return result;
+                }
+            }
+        }
+        throw new IOException("No Git repository found in " + extractedFolder);
     }
 }
